1ce0f01ea7d95359c52434ba0366ead953d3270e
alvarofpp <alvarofepipa@gmail.com>
2020-07-22 22:27:29 -0300

    Bug fixed

--- src/helpers.php
+++ src/helpers.php
@@ -5,11 +5,15 @@ if (!function_exists('mask')) {
      * Applies mask in the value.
      *
      * @param string $mask
-     * @param string $value
+     * @param $value
      * @return string
      */
-    function mask(string $mask, string $value): string
+    function mask(string $mask, $value): string
     {
+        if (is_null($value)) {
+            return '';
+        }
+
         $value = str_replace(" ", "", $value);
         $valueArray = str_split($value);
         return Illuminate\Support\Str::replaceArray('#', $valueArray, $mask);
